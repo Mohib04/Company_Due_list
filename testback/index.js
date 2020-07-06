1c01e983bd753824cae569cf28f17de524b0b8f1
Mohib4 <mohib.agu@gmail.com>
2020-07-06 10:24:31 +0600

    Adding Port

--- testback/index.js
+++ testback/index.js
@@ -4,10 +4,19 @@ const app = express();
 
 const port = 2000;
 
+app.get('/gm', (req, res) => {
+    return res.send("This page only for Genaral Manager sir MD Alomgir Mirdha");
+});
+app.get('/m', (req, res) => {
+    return res.send("This page only for Manger MD Pintu");
+});
 app.get('/', (req, res) => {
-    return res.send("Hello World");
+    return res.send("Welcome to Bismillah Auto Solution");
+});
+app.get('/signup', (req, res) => {
+    return res.send("This is signup Page");
 });
 
 app.listen(port, () => {
-    return console.log("Bismillah Auto Solution");
+    return console.log("App is runnig at 2000 port");
 });
